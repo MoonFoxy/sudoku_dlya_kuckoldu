f3a8772d72e7aa79f43e07a225d3dd13e3a552a6
OtezKnstn <otezknstn@gmail.com>
2021-12-08 16:10:09 +0300

    backend

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# configuration
+DEBUG = False
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app)
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('ti peedoor!')
+
+if __name__ == '__main__':
+    app.run()
