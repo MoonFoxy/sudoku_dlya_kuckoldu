28897834d317e36d553fab26b4d871961bf9e44f
MoonFoxy <ilyabalakirev2003@gmail.com>
2021-12-30 18:54:00 +0300

    fuck this CORS

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from types import NoneType
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 from sudoku import Sudoku
 from solver import solve_sudoku
@@ -10,6 +10,7 @@ CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/sudoku/generate', methods=['POST'])
+@cross_origin()
 def sudoku_generation():
     data = request.get_json()
     size, dif = data.get('size'), data.get('dif')
@@ -17,24 +18,25 @@ def sudoku_generation():
         try:
             size, dif = int(size), int(dif)
         except ValueError:
-            return jsonify({ 'error': 'NAN' }).headers.add('Access-Control-Allow-Origin', '*'), 404
+            return jsonify({ 'error': 'NAN' }), 404
 
         if size in range(2, 6) and dif in range(0, 6):
             try:
                 matrix = Sudoku(size, dif)
             except ValueError:
-                return jsonify({ 'error': 'Sudoku generation error' }).headers.add('Access-Control-Allow-Origin', '*'), 404
+                return jsonify({ 'error': 'Sudoku generation error' }), 404
 
             matrixString = ''.join(str(ele) for sub in matrix.masked_grid for ele in sub)
             matrixString = matrixString.join(str(int(ele != 0)) for sub in matrix.masked_grid for ele in sub)
             return jsonify({ 
                 'matrix': matrix.masked_grid,
                 'matrixString': matrixString
-                }).headers.add('Access-Control-Allow-Origin', '*'), 200
+                }), 200
 
-    return jsonify({ 'error': 'Invalid input' }).headers.add('Access-Control-Allow-Origin', '*'), 404
+    return jsonify({ 'error': 'Invalid input' }), 404
 
 @app.route('/sudoku/numsol', methods=['POST'])
+@cross_origin()
 def check_solutions():
     data = request.get_json()
     size, matrix = data.get('size'), data.get('matrix')
@@ -42,7 +44,7 @@ def check_solutions():
         try:
             size = int(size)
         except ValueError:
-            return jsonify({ 'error': 'NAN' }).headers.add('Access-Control-Allow-Origin', '*'), 404
+            return jsonify({ 'error': 'NAN' }), 404
             
         solutions = 0
         if size in range(2, 6):
@@ -52,11 +54,11 @@ def check_solutions():
                     if solutions > 100:
                         break
             except:
-                return jsonify({ 'error': 'Counting error' }).headers.add('Access-Control-Allow-Origin', '*'), 404
+                return jsonify({ 'error': 'Counting error' }), 404
 
-            return jsonify({ 'sol': solutions }).headers.add('Access-Control-Allow-Origin', '*'), 200
+            return jsonify({ 'sol': solutions }), 200
 
-    return jsonify({ 'error': 'Invalid input' }).headers.add('Access-Control-Allow-Origin', '*'), 404
+    return jsonify({ 'error': 'Invalid input' }), 404
 
 if __name__ == '__main__':
   app.run(debug = True)
