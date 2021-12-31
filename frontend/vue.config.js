432ef2f81486b3bb1e80d6625d66d6f2486647a7
MoonFoxy <IlyaBalakirev2003@gmail.com>
2021-12-31 13:56:54 +0300

    Change proxy target

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/sudoku': {
-        target: 'http://127.0.0.1:5000/',
+        target: 'https://api.tsukiko.tech',
         changeOrigin: true,
       },
     },
